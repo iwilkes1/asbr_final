53a57bb858b314bf9d889cc8c5053675c33200ea
Ian Wilkes <ianm.wilkes@gmail.com>
2015-05-13 02:42:13 -0400

    final filtering code

--- filter.py
+++ filter.py
@@ -13,8 +13,9 @@ problem in the final to get the predicted x and y positions.
 import numpy
 import random
 import scipy.stats
+import bisect
 
-NUM_PARTICLES = 15000
+NUM_PARTICLES = 150000
 DEFAULT_ACTION = [0.5, 0]
 
 
@@ -62,12 +63,12 @@ def get_weights(particles, sensor_reading=None, sensor_stdev=SENSOR_STDEV):
     x_read = sensor_reading[0]
     y_read = sensor_reading[1]
 
-    x_dist = scipi.stats.norm(x_read, sensor_stdev)
-    y_dist = scipi.stats.norm(y_read, sensor_stdev)
+    x_dist = scipy.stats.norm(x_read, sensor_stdev)
+    y_dist = scipy.stats.norm(y_read, sensor_stdev)
     
     for particle in particles:
-        x_prob = x_dist.pdf(particle[0])
-        y_prob = y_dist.pdf(particle[1])
+        x_prob = x_dist.pdf(particle[0])[0]
+        y_prob = y_dist.pdf(particle[1])[0]
         weights.append(x_prob * y_prob)
     return weights
 
@@ -81,10 +82,9 @@ def resample_on_weights(particles, weights):
     for i in xrange(len(particles)):
         sum += weights[i]
         cutoffs.append(sum)
-
     for i in xrange(len(particles)):
         search_value = random.uniform(0.0, sum)
-        index = bisect_right(cutoffs, search_value)
+        index = bisect.bisect_right(cutoffs, search_value)
         if index:
             new_particles.append(particles[index])
     return new_particles
@@ -113,15 +113,25 @@ if __name__ == '__main__':
     main portion of code.
     """
     x_0 = get_initial_particles()
+    print len(x_0), "x_0"
     x_1_prime = move_particles(x_0)
+    print len(x_1_prime), "x_1'"
     w_1 = get_weights(x_1_prime, (3.1, 5.3))
+    print len(w_1), "w_1'"
     x_1 = resample_on_weights(x_1_prime, w_1)
+    print len(x_1), "x_1"
     # no measurements for 2 and 3
+
     x_2_prime = move_particles(x_1)
+    print len(x_2_prime), "x_2'"
     x_3_prime = move_particles(x_2_prime)
+    print len(x_3_prime), "x_3'"
     x_4_prime = move_particles(x_3_prime)
+    print len(x_4_prime), "x_4'"
     w_4 = get_weights(x_4_prime, (4.7,6.1))
+    print len(w_4), "w_4"
     x_4 = resample_on_weights(x_4_prime, w_4)
+    print len(x_4), "x_4"
     print get_mean_x(x_4), get_mean_y(x_4)
     
 
